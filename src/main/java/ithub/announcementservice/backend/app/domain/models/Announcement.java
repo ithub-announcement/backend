74bb0f4a86d9fe3f4ccc885f77d184de50d1b463
Дмитрий Чехонадских <loseex@vk.com>
2024-04-01 18:11:38 +0500

    hot fix

--- src/main/java/ithub/announcementservice/backend/app/domain/models/Announcement.java
+++ src/main/java/ithub/announcementservice/backend/app/domain/models/Announcement.java
@@ -19,6 +19,7 @@ public class Announcement {
     @Column(nullable = false)
     private String title;
 
+    @Column(columnDefinition = "TEXT")
     private String content;
 
     private String authorId;
